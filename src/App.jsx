f5d292153c433d12b4a5fc3e2ef4eba02d71d9c9
wzx <wfkmsx@gmail.com>
2025-12-31 16:18:31 +0800

    refactor: extract Square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 
+function Square() {
+  return <div className="cell" />
+}
+
 function App() {
   return (
     <div className="game-container">
       <h1>井字棋</h1>
       <div className="board">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
-          <div key={index} className="cell" />
+          <Square key={index} />
         ))}
       </div>
     </div>
